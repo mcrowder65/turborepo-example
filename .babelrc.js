d726eee70ddcb9694dfeb50c8e1d82ce08dbac32
Matt Crowder <mcrowder65@gmail.com>
2022-03-06 19:39:41 -0600

    wip

--- .babelrc.js
+++ .babelrc.js
@@ -9,11 +9,33 @@ function rename({ path, folder, filename }) {
   }
 }
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 3, // or 2,
+        targets: {
+          firefox: "64", // or whatever target to choose .
+        },
+      },
+    ],
+    "@babel/preset-react",
+  ],
   plugins: [
     function myCustomPlugin() {
       return {
         visitor: {
+          CallExpression(path) {
+            if (path.node.callee.name === "require") {
+              if (this.filename.includes("/rs/")) {
+                if (path.node.arguments[0].value.indexOf("src/") === 0) {
+                  path.node.arguments[0].value = `rs/${path.node.arguments[0].value}`
+                  debugger
+                }
+              }
+            }
+          },
           ImportDeclaration(path) {
             rename({ path, filename: this.filename, folder: "shared/" })
             rename({ path, filename: this.filename, folder: "shared-two/" })
--- apps/rs/src/page.js
+++ apps/rs/src/page.js
@@ -1,3 +1,4 @@
+import React from "react"
 export default function Page() {
   return <div>I am the page!</div>
 }
